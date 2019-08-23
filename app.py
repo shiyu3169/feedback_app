51ff8f43406d614122167d88c506269080832590
Shiyu Wang <shiyu3169@gmail.com>
2019-08-23 10:43:47 -0400

    database halfly done

--- app.py
+++ app.py
@@ -1,6 +1,35 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:cliff92711@localhost/lexus'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.String(200))
+    dealer = db.Column(db.String(200), unique=True)
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+    def __init__(self, customer, dealer, rating, comments):
+        self.customer = customer
+        self.dealer = dealer
+        self.rating = rating
+        self.comments = comments
 
 
 @app.route("/")
@@ -21,5 +50,4 @@ def submit():
 
 
 if __name__ == '__main':
-    app.debug = True
     app.run()
